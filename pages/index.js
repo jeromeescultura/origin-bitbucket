8d87167cfb5b5cf8a4fabf6cb8880ceec3757f07
Jerome Escultura <jescultura@deloitte.com>
2022-06-06 10:21:18 +0800

    Removed react-bottom package

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from "react";
 import Image from "next/image";
-
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import ProductContainer from "../containers/ProductContainer";
