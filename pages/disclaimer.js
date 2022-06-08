372c0ac033c440ec06bdc57e1881840863128f19
Jerome Escultura <jescultura@deloitte.com>
2022-06-08 11:34:13 +0800

    finalize disclaimer page

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -2,9 +2,6 @@ import { Button } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-import ContentContainer from "../containers/ContentContainer";
-import Navbar from "../components/Navbar";
-
 const disclaimer = () => {
   const router = useRouter();
   const handleClick = (e, url) => {
@@ -12,18 +9,33 @@ const disclaimer = () => {
     router.push(url);
   };
   return (
-    <ContentContainer>
-      <Navbar />
-      <div className="flex flex-col md:flex-row md:gap-8">
-        <div className="space-y-8">
-          <h2 className="text-secondaryBG font-bold">
+    <div className="w-[90vw] sm:w-[80vw] md:w-[90vw] max-w-[1140px] mx-auto">
+      <div className="w-16 lg:w-20 cursor-pointer pt-4 lg:pt-8">
+        <Image
+          src="/images/origin-logo.svg"
+          width={90}
+          height={90}
+          objectFit="contain"
+          alt="origin-logo"
+          onClick={handleClick}
+        />
+      </div>
+      <div className="inline-grid md:grid-cols-3 gap-8 xl:gap-32 mt-8 lg:mt-24">
+        <div className="space-y-8 lg:space-y-12 col-span-2">
+          <h2 className="text-primaryText font-bold">
             Getting started with your Clean Energy Consultant
           </h2>
           <div>
             <p className="font-medium subtitle">Eligibility</p>
-            <p className="my-4">
+            <p className="my-4 lg:hidden">
               Our small & medium business Clean Ambition Program is currently
               running a trial only in <strong>South Australia</strong> and{" "}
+              <strong>Victoria.</strong>
+            </p>
+            <p className="hidden lg:block my-6">
+              Our online Clean Energy Consultant is currently able to identify
+              clean energy altertives for small & medium businesses in{" "}
+              <strong>South Australia, Queensland</strong> and{" "}
               <strong>Victoria.</strong>
             </p>
             <p>
@@ -32,17 +44,16 @@ const disclaimer = () => {
               let us know by indicating your expression of interest.
             </p>
           </div>
-          <div className="flex flex-col gap-4 border-2 md:w-3/4">
+          <div className="flex flex-col gap-4 md:w-2/3 lg:flex-row lg:w-full xl:pr-12">
             <Button
               variant="contained"
               color="primary"
               size="large"
+              className="lg:w-full"
               onClick={(e) => handleClick(e, "/assessment")}
               style={{
                 borderRadius: 200,
                 boxShadow: "none",
-                paddingLeft: "2rem",
-                paddingRight: "2rem",
               }}
             >
               Continue
@@ -51,30 +62,37 @@ const disclaimer = () => {
               variant="outlined"
               size="large"
               color="error"
+              className="lg:w-full"
               onClick={(e) => handleClick(e, "/signup")}
               style={{
                 borderRadius: 200,
                 boxShadow: "none",
-                paddingLeft: "2rem",
-                paddingRight: "2rem",
               }}
             >
               Submit expression of interest
             </Button>
           </div>
         </div>
-        <div className="hidden w-[80vw] md:inline">
+        <div className="relative hidden md:inline md:h-[250px] lg:h-[350px] xl:w-[350px] xl:h-full">
           <Image
             src="/images/disclaimer-tree.png"
-            width={800}
-            height={940}
+            layout="fill"
             objectFit="cover"
             alt="trees"
             className="rounded-2xl"
           />
         </div>
+
+        {/* <Image
+            src="/images/disclaimer-tree.png"
+            width={800}
+            height={800}
+            objectFit="cover"
+            alt="trees"
+            className="rounded-2xl"
+          /> */}
       </div>
-    </ContentContainer>
+    </div>
   );
 };
 
