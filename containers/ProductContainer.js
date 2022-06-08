aab6bfdec73a1e393b91c923ee4e0e3155e22c3a
Jerome Escultura <jescultura@deloitte.com>
2022-06-08 12:28:15 +0800

    Updated products contents

--- containers/ProductContainer.js
+++ containers/ProductContainer.js
@@ -2,10 +2,58 @@ import Slider from "react-slick";
 import ProductCard from "../components/ProductCard";
 
 function ProductContainer() {
+  const productDetails = [
+    {
+      title: "Origin Go Zero",
+      icon: "/icons/products/go-zero.svg",
+      impact: "Through offsetting your energy use",
+      plan: "Available on any Origin Energy plan",
+      siteChanges: "None",
+      contracts: "None",
+      benefits: ["All standard clean business club benefits"],
+    },
+    {
+      title: "GreenPower",
+      icon: "/icons/products/greenpower.svg",
+      impact: "Through funding renewable generators",
+      plan: "Available on any Origin Energy plan",
+      siteChanges: "None",
+      contracts: "None",
+      benefits: [
+        "All standard clean business club benefits",
+        "Tariff review to reduce your energy costs",
+      ],
+    },
+    {
+      title: "Solar",
+      icon: "/icons/products/solar.svg",
+      impact: "Through using self generated renewable energy",
+      plan: "Competitive solar plans available",
+      siteChanges: "Yes",
+      contracts: "None",
+      benefits: [
+        "All standard clean business club benefits",
+        "Mimise energy costs by using clean, self generated solar power",
+      ],
+    },
+    {
+      title: "Net-Zero strategy review",
+      icon: "/icons/products/net-zero.svg",
+      impact: "Have input in the development of impact reduction services",
+      plan: "Available on any Origin Energy plan",
+      siteChanges: "None",
+      contracts: "None",
+      benefits: [
+        "All standard clean business club benefits",
+        "Have your say in future & upcoming solutions",
+      ],
+    },
+  ];
+
   var settings = {
     centerMode: false,
     infinite: false,
-    slidesToShow: 3,
+    slidesToShow: 4,
     speed: 500,
     initialSlide: 1,
     arrows: false,
@@ -60,9 +108,18 @@ function ProductContainer() {
   return (
     <div className="py-8 lg:w-[80vw] mx-auto mb-16 sm:mt-[110px] md:mt-[48px]">
       <Slider {...settings}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {productDetails.map((detail, index) => (
+          <ProductCard
+            key={index}
+            title={detail.title}
+            icon={detail.icon}
+            impact={detail.impact}
+            plan={detail.plan}
+            siteChanges={detail.siteChanges}
+            contracts={detail.contracts}
+            benefits={detail.benefits}
+          />
+        ))}
       </Slider>
     </div>
   );
