59fc4e280cbbdf0b467683fbc65be0f3b76c7a3b
Jerome Escultura <jescultura@deloitte.com>
2022-06-24 10:15:40 +0800

    replaced prod URL

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://origin-shift.netlify.app";
+  : "https://origin-shift-mvp.netlify.app";
