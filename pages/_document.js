96948a9bab894be8235025d3779d7ca34b05b320
Jerome Escultura <jescultura@deloitte.com>
2022-06-30 13:06:54 +0800

    remove tracking

--- pages/_document.js
+++ pages/_document.js
@@ -30,47 +30,6 @@ export default class MyDocument extends Document {
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {this.props.emotionStyleTags}
           {/* Analytics */}
-          {/* Adobe */}
-          <script
-            src="https://assets.adobedtm.com/52e6c31223bb/da58a2b2287a/launch-EN92bec34db9c84af69ed78945eed39c3d-development.min.js"
-            async
-          ></script>
-          <script
-            src="https://assets.adobedtm.com/52e6c31223bb/da58a2b2287a/launch-ENf697fd9614a3432f8f0fb26672a1149d.min.js"
-            async
-          ></script>
-          {/* Google */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=DC-11918918"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'DC-11918918');
-          </script>
-          {/* Facebook */}
-
-          <script>
-            !function(f,b,e,v,n,t,s) if(f.fbq)return;n=f.fbq=function()
-            {n.callMethod
-              ? n.callMethod.apply(n, arguments)
-              : n.queue.push(arguments)}
-            ; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s) (window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js'); fbq('init',
-            '1041470032612059'); fbq('track', 'PageView');
-          </script>
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style="display:none"
-              src="https://www.facebook.com/tr?id=1041470032612059&ev=PageView&noscript=1"
-            />
-          </noscript>
         </Head>
         <body>
           <Main />
