54827fe496a2799832350b3ac8e23b87eb8fe29d
wpnenriquez <wenriquez@deloitte.com>
2022-07-12 14:45:49 +0800

    created variable to store source and version in recommend page

--- pages/recommend.js
+++ pages/recommend.js
@@ -46,6 +46,21 @@ const Recommend = () => {
   var duration = require("dayjs/plugin/duration");
   dayjs.extend(duration);
 
+  const source =
+    JSON.parse(
+      typeof window !== "undefined" && window.localStorage.getItem("SOURCE")
+    ) || null;
+
+  const version =
+    JSON.parse(
+      typeof window !== "undefined" && window.localStorage.getItem("VERSION")
+    ) || null;
+
+  useEffect(() => {
+    console.log("SOURCE: ", source);
+    console.log("VERSION: ", version);
+  }, []);
+
   const storedStepOneData =
     JSON.parse(
       typeof window !== "undefined" &&
@@ -736,6 +751,8 @@ const Recommend = () => {
                       btn1={btn1}
                       btn2={btn2}
                       btn3={btn3}
+                      source={source}
+                      version={version}
                     />
                   </div>
                   <div className="break-inside-avoid">
