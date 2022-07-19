a6e94d85e42530a8dc4b817a43d0909ab0ee873b
wpnenriquez <wenriquez@deloitte.com>
2022-07-19 10:50:57 +0800

    added " or as a stand-alone product" for solar pledge dropdown in signup page

--- pages/signup.js
+++ pages/signup.js
@@ -176,7 +176,7 @@ function Signup() {
                     <div className="grid grid-rows-3 text-left">
                       <div className="grid grid-cols-2 items-center border-t py-2">
                         <p className="font-medium">Plan</p>
-                        <p>Available on any Origin Electricity plan</p>
+                        <p>Available on any Origin Electricity plan{product === 'solar' ? ' or as a stand-alone product' : ''}</p>
                       </div>
                       <div className="grid grid-cols-2 items-center border-t py-2">
                         <p>Site changes</p>
