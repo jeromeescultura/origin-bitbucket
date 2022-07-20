5f30dffd7c6f2d28a5d95e51f72dcbffbe8e815e
Jerome Escultura <jescultura@deloitte.com>
2022-07-20 10:53:15 +0800

    Added fav icons

--- pages/_document.js
+++ pages/_document.js
@@ -13,8 +13,34 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="theme-color" content="#ffffff" />
+          <link rel="shortcut icon" href="favs/favicon.ico" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favs/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favs/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favs/favicon-16x16.png"
+          />
+          <link rel="manifest" href="favs/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="favs/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -34,15 +60,15 @@ export default class MyDocument extends Document {
           {this.props.emotionStyleTags}
           {/* Analytics */}
           {/* Adobe */}
-          <script
+          {/* <script
             src="https://assets.adobedtm.com/52e6c31223bb/da58a2b2287a/launch-EN92bec34db9c84af69ed78945eed39c3d-development.min.js"
             async
-          ></script>
+          ></script> */}
 
-          {/* <script
+          <script
             src="https://assets.adobedtm.com/52e6c31223bb/da58a2b2287a/launch-ENf697fd9614a3432f8f0fb26672a1149d.min.js"
             async
-          ></script> */}
+          ></script>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           {/* Google Tag Manager */}
           {/* <Script
